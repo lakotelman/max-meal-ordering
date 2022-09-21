f1d8f2a6e4a29ca12f9a974848d1a49685b60903
lakotelman <la.n.kot@pm.me>
2022-09-21 12:53:42 -0800

    button bump

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,34 @@
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const [btnBump, setBtnBump] = useState(false);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+    const timer = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const buttonClasses = `${styles.button} ${btnBump ? styles.bump : ""}`;
 
   const cartItemsCount = cartCtx.items.reduce((curr, item) => {
     return curr + item.amount;
   }, 0);
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={styles.CartIcon}>
         <CartIcon />
       </span>
