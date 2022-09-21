1cb9cc1e3b4e3a846afbb8bc26c1518951a4c6ed
lakotelman <la.n.kot@pm.me>
2022-09-21 10:21:16 -0800

    Finishing Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import styles from "./Header.module.css"
 import mealsImage from "../../assets/banquetsetup.jpg"
+import HeaderCartButton from "./HeaderCartButton"
 
 const Header = props => { 
 return (
     <>
 <header className={styles.header}>
 <h1>Dinin'</h1>
-<button>Cart</button>
+<HeaderCartButton/>
 </header>
 <div className={styles["main-image"]}><img src={mealsImage} alt="A table with delicious food"/></div>
     </>
