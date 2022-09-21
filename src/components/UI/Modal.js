1d5947e77019bd4a3d91300b6b03c5ba253408af
lakotelman <la.n.kot@pm.me>
2022-09-21 11:23:14 -0800

    Modal component config

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,23 @@
+import { ReactDOM } from "react"
+import styles from "./Modal.module.css"
+import { Fragment } from "react"
+
+const Backdrop = props => {
+    return <div className={styles.backdrop}></div>
+ }
+
+const ModalOverlay = props => { 
+    return <div className={styles.modal}>
+        <div className={styles.overlay}>{props.children}</div>
+    </div>
+}
+const portalElement = document.getElementById("overlays")
+
+const Modal = props => { 
+    return (<Fragment>
+        {ReactDOM.createPortal(<Backdrop/>, portalElement)}
+        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </Fragment>)
+
+}
+export default Modal
