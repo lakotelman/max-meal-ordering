2ff1f89fb3a1b9cbc8b040cebe8b721115ec637e
lakotelman <la.n.kot@pm.me>
2022-09-21 09:44:31 -0800

    components structure

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import styles from "./Header"
+const Header = props => { 
+return (
+    <>
+<header>
+<h1>Dinin'</h1>
+<button>Cart</button>
+</header>
+<div></div>
+    </>
+)
+}
+
+export default Header
